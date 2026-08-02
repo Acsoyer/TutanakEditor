ff579ba87fd4347f6bffbed547f5eaaedb139b60
caner-soyer <canersoyer@gmail.com>
2026-08-02 13:00:39 +0100

    v21

---
+++ v18-result-variants.js
@@ -0,0 +1,112 @@
+window.ARBSYS_RESULT_VARIANTS = {
+  "schemaVersion": 1,
+  "groups": [
+    {
+      "id": "son-tutanak-elverissiz",
+      "documentType": "Son Tutanak",
+      "title": "Arabuluculuğa Elverişli Olunmaması",
+      "options": [
+        {
+          "title": "Arabuluculuğa Elverişli Olunmaması",
+          "html": "<div>Başvurusu konusu uyuşmazlığın, arabuluculuğa elverişli olmayıp \"diğer tür uyuşmazlıklarda dava şartı\" kapsamı dışında kaldığı tespit edildiği ve hazırlık aşamasında yapılan görüşme neticesinde bu tespit doğrulanmış olduğu için<b>&nbsp;arabuluculuk süreci \"arabuluculuğa elverişli olunmaması\" olarak sonlandırılmış ve işbu belge arabulucu tarafından re'sen düzenlenmiştir.</b><b>&nbsp;</b><b>XX.XX.20XX</b></div>",
+          "text": "Başvurusu konusu uyuşmazlığın, arabuluculuğa elverişli olmayıp \"diğer tür uyuşmazlıklarda dava şartı\" kapsamı dışında kaldığı tespit edildiği ve hazırlık aşamasında yapılan görüşme neticesinde bu tespit doğrulanmış olduğu için arabuluculuk süreci \"arabuluculuğa elverişli olunmaması\" olarak sonlandırılmış ve işbu belge arabulucu tarafından re'sen düzenlenmiştir. XX.XX.20XX"
+        }
+      ]
+    },
+    {
+      "id": "son-tutanak-konusuz-kalma",
+      "documentType": "Son Tutanak",
+      "title": "Konusuz Kalma / Başvurucunun Vazgeçmesi",
+      "options": [
+        {
+          "title": "Konusuz Kalma / Başvurucunun Vazgeçmesi",
+          "html": "<div>İşbu tutanak 6325 sayılı kanunun 17. maddesi uyarınca arabulucu tarafından düzenlenmiş olup <b>\"başvurucunun vazgeçme beyanı\" / \"uyuşmazlığın konusuz kalması\" nedeniyle arabuluculuk gorüşmelerine başlanmadan dosya \"Konusuz Kalma - Başvurucunun Vazgeçmesi\" olarak kapatılmış </b>ve işbu belge arabulucu tarafından re'sen düzenlenmiştir.<b>&nbsp;</b><b>XX.XX.20XX</b></div>",
+          "text": "İşbu tutanak 6325 sayılı kanunun 17. maddesi uyarınca arabulucu tarafından düzenlenmiş olup \"başvurucunun vazgeçme beyanı\" / \"uyuşmazlığın konusuz kalması\" nedeniyle arabuluculuk gorüşmelerine başlanmadan dosya \"Konusuz Kalma - Başvurucunun Vazgeçmesi\" olarak kapatılmış ve işbu belge arabulucu tarafından re'sen düzenlenmiştir. XX.XX.20XX"
+        }
+      ]
+    },
+    {
+      "id": "son-tutanak-yetkisizlik",
+      "documentType": "Son Tutanak",
+      "title": "Yetkisizlik",
+      "options": [
+        {
+          "title": "Yetkisizlik",
+          "html": "<div>İşbu tutanak 6325 sayılı kanunun 17. maddesi uyarınca arabulucu tarafından düzenlenmiş olup di<b>ğer tarafın&nbsp;\"yetki itirazında bulunulmuş olması\" nedeniyle </b>6325&nbsp;Sayılı Kanun 'un 18/A maddesinin 8. fıkrası <b>uyarınca </b>yetki itirazı hakkında gerekli inceleme, değerlendirme ve işlemlerin yapılabilmesi için<b> \"Yetkisizlik\" olarak kapatılmış </b>ve işbu belge <b>arabulucu tarafından re'sen düzenlenmiştir.</b><b> </b><b>XX.XX.20XX</b></div>",
+          "text": "İşbu tutanak 6325 sayılı kanunun 17. maddesi uyarınca arabulucu tarafından düzenlenmiş olup di ğer tarafın \"yetki itirazında bulunulmuş olması\" nedeniyle 6325 Sayılı Kanun 'un 18/A maddesinin 8. fıkrası uyarınca yetki itirazı hakkında gerekli inceleme, değerlendirme ve işlemlerin yapılabilmesi için \"Yetkisizlik\" olarak kapatılmış ve işbu belge arabulucu tarafından re'sen düzenlenmiştir. XX.XX.20XX"
+        }
+      ]
+    },
+    {
+      "id": "son-tutanak-sehven",
+      "documentType": "Son Tutanak",
+      "title": "Sehven Kayıt",
+      "options": [
+        {
+          "title": "Sehven Kayıt 1",
+          "html": "<div>Başvurusu konusu işçilik alacaklarından prim - ikramiye alacağı değerlendirilerek arabuluculuk süreci başlatılmış ise de görüşmeler sırasında uyuşmazlık konusunun SGK ya eksik ödenen primlere ilişkin olduğu anlaşıldığından ve bu hususta arabuluculuk görüşmesi yapılamayacağından <b>arabuluculuk süreci \"sehven kayıt\" olarak sonlandırılmış ve işbu belge arabulucu tarafından re'sen düzenlenmiştir.</b><b>&nbsp;</b><b>XX.XX.20XX</b></div>",
+          "text": "Başvurusu konusu işçilik alacaklarından prim - ikramiye alacağı değerlendirilerek arabuluculuk süreci başlatılmış ise de görüşmeler sırasında uyuşmazlık konusunun SGK ya eksik ödenen primlere ilişkin olduğu anlaşıldığından ve bu hususta arabuluculuk görüşmesi yapılamayacağından arabuluculuk süreci \"sehven kayıt\" olarak sonlandırılmış ve işbu belge arabulucu tarafından re'sen düzenlenmiştir. XX.XX.20XX"
+        },
+        {
+          "title": "Sehven Kayıt 2",
+          "html": "<div>Uyuşmazlık konusunun arabuluculuga elverişli olmadığı anlaşıldığından ve bu hususta arabuluculuk görüşmesi yapılamayacağından <b>arabuluculuk süreci \"sehven kayıt\" olarak sonlandırılmış ve işbu belge arabulucu tarafından re'sen düzenlenmiştir.</b><b>&nbsp;</b><b>XX.XX.20XX</b></div>",
+          "text": "Uyuşmazlık konusunun arabuluculuga elverişli olmadığı anlaşıldığından ve bu hususta arabuluculuk görüşmesi yapılamayacağından arabuluculuk süreci \"sehven kayıt\" olarak sonlandırılmış ve işbu belge arabulucu tarafından re'sen düzenlenmiştir. XX.XX.20XX"
+        },
+        {
+          "title": "Sehven Kayıt 3",
+          "html": "<div>Ortada gerçek bir uyuşmazlık olmadığı anlaşıldığından ve bu hususta arabuluculuk görüşmesi yapılamayacağından <b>arabuluculuk süreci \"sehven kayıt\" olarak sonlandırılmış ve işbu belge arabulucu tarafından re'sen düzenlenmiştir.</b><b>&nbsp;</b><b>XX.XX.20XX</b></div>",
+          "text": "Ortada gerçek bir uyuşmazlık olmadığı anlaşıldığından ve bu hususta arabuluculuk görüşmesi yapılamayacağından arabuluculuk süreci \"sehven kayıt\" olarak sonlandırılmış ve işbu belge arabulucu tarafından re'sen düzenlenmiştir. XX.XX.20XX"
+        }
+      ]
+    },
+    {
+      "id": "toplanti-gorusme-erteleme",
+      "documentType": "Toplantı Tutanağı",
+      "title": "Görüşme Sonunda Erteleme",
+      "options": [
+        {
+          "title": "Görüşme Sonunda Erteleme 1",
+          "html": "<div>Taraflarca uyuşmazlık konusu müzakere edildikten sonra, yeniden değerlendirilmek üzere, <b>XX.XX.20XX ……. günü saat XX.XX’da</b> yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, <b>üç (3)</b> nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. <b>XX.XX.20XX</b></div>",
+          "text": "Taraflarca uyuşmazlık konusu müzakere edildikten sonra, yeniden değerlendirilmek üzere, XX.XX.20XX ……. günü saat XX.XX’da yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, üç (3) nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. XX.XX.20XX"
+        },
+        {
+          "title": "Görüşme Sonunda Erteleme 2",
+          "html": "<div><div></div>Taraflarca uyuşmazlık konusu müzakere edildikten sonra, işverenin ................................ ................. ............. olduğu anlaşıldığından diğer tarafın da toplantıya davet edilebilmesi amacıyla ve uyuşmazlık konularının yeniden değerlendirilmek üzere, <b>XX.XX.20XX ……. günü saat XX.XX’da</b> yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, <b>üç (3)</b> nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. <b>XX.XX.20XX</b></div>",
+          "text": "Taraflarca uyuşmazlık konusu müzakere edildikten sonra, işverenin ................................ ................. ............. olduğu anlaşıldığından diğer tarafın da toplantıya davet edilebilmesi amacıyla ve uyuşmazlık konularının yeniden değerlendirilmek üzere, XX.XX.20XX ……. günü saat XX.XX’da yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, üç (3) nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. XX.XX.20XX"
+        },
+        {
+          "title": "Görüşme Sonunda Erteleme 3",
+          "html": "<div><div></div><div></div>Taraflarca uyuşmazlık konusu müzakere edildikten sonra, işverenin ........................................... değil ............. ....................... olduğu anlaşıldığından diğer tarafın da toplantıya davet edilebilmesi amacıyla ve uyuşmazlık konularının yeniden değerlendirilmek üzere,&nbsp;<b>XX.XX.20XX ……. günü saat XX.XX’da</b> yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, <b>üç (3)</b> nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. <b>XX.XX.20XX</b></div>",
+          "text": "Taraflarca uyuşmazlık konusu müzakere edildikten sonra, işverenin ........................................... değil ............. ....................... olduğu anlaşıldığından diğer tarafın da toplantıya davet edilebilmesi amacıyla ve uyuşmazlık konularının yeniden değerlendirilmek üzere, XX.XX.20XX ……. günü saat XX.XX’da yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, üç (3) nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. XX.XX.20XX"
+        }
+      ]
+    },
+    {
+      "id": "toplanti-gorusmeyok-erteleme",
+      "documentType": "Toplantı Tutanağı",
+      "title": "Görüşme Yapılmadan Erteleme",
+      "options": [
+        {
+          "title": "Görüşme Yapılmadan Erteleme 1",
+          "html": "<div>Arabuluculuk görüşmelerindeki ilk toplantı, .................................... nin erteleme talebi ve ........................................................ kabulü sonucunda, <b>XX.XX.20XX ……. günü saat XX.XX’da</b> yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, <b>üç (3)</b> nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. <b>XX.XX.20XX</b></div>",
+          "text": "Arabuluculuk görüşmelerindeki ilk toplantı, .................................... nin erteleme talebi ve ........................................................ kabulü sonucunda, XX.XX.20XX ……. günü saat XX.XX’da yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, üç (3) nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. XX.XX.20XX"
+        },
+        {
+          "title": "Görüşme Yapılmadan Erteleme 2",
+          "html": "<div>Toplantı tarafların ortak kararıyla, <b>XX.XX.20XX ……. günü saat XX.XX’da</b> yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, <b>üç (3)</b> nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. <b>XX.XX.20XX</b></div>",
+          "text": "Toplantı tarafların ortak kararıyla, XX.XX.20XX ……. günü saat XX.XX’da yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, üç (3) nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. XX.XX.20XX"
+        },
+        {
+          "title": "Görüşme Yapılmadan Erteleme 3",
+          "html": "<div><div></div>Taraflarla yapılan görüşmelerde, işverenin ................................ ................. ............. olduğu anlaşıldığından diğer tarafın da toplantıya davet edilebilmesi amacıyla, <b>XX.XX.20XX ……. günü saat XX.XX’da</b> yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, <b>üç (3)</b> nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. <b>XX.XX.20XX</b></div>",
+          "text": "Taraflarla yapılan görüşmelerde, işverenin ................................ ................. ............. olduğu anlaşıldığından diğer tarafın da toplantıya davet edilebilmesi amacıyla, XX.XX.20XX ……. günü saat XX.XX’da yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, üç (3) nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. XX.XX.20XX"
+        },
+        {
+          "title": "Görüşme Yapılmadan Erteleme 4",
+          "html": "<div><div></div>Taraflarla yapılan görüşmelerde, işverenin ........................................... değil ............. ....................... olduğu anlaşıldığından diğer tarafın da toplantıya davet edilebilmesi amacıyla, <b>XX.XX.20XX ……. günü saat XX.XX’da</b> yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, <b>üç (3)</b> nüsha olarak hazırlanmış <b>taraflarla birlikte imza altına alınarak</b> birer nüshası taraflara teslim edilmiştir. <b>XX.XX.20XX</b></div>",
+          "text": "Taraflarla yapılan görüşmelerde, işverenin ........................................... değil ............. ....................... olduğu anlaşıldığından diğer tarafın da toplantıya davet edilebilmesi amacıyla, XX.XX.20XX ……. günü saat XX.XX’da yapılacak bir sonraki toplantıya ertelenmiştir. İşbu tutanak, üç (3) nüsha olarak hazırlanmış taraflarla birlikte imza altına alınarak birer nüshası taraflara teslim edilmiştir. XX.XX.20XX"
+        }
+      ]
+    }
+  ]
+};
